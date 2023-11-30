1f14a8ffadfa56b05156d454197d2d13bab9fa95
Rodrigo Vásquez de Velasco Gonzales Vigil <87100608+rodrigovdv01@users.noreply.github.com>
2023-11-30 15:30:25 -0500

    getMisPedidos

--- controllers/PedidoControllers.js
+++ controllers/PedidoControllers.js
@@ -85,7 +85,7 @@ const createPedido = async (req, res) => {
 const getMisPedidos = async (req, res) => {
   try {
     // Obtén el token de la cookie
-    const authToken = localStorage.getItem("authToken");
+    const authToken = req.cookies.authToken;
 
     if (!authToken) {
       return res.status(401).json({ message: "No estás autenticado" });
