0617cb753ed6bc9fe382b5e7f942cd69bcda7340
Rodrigo Vásquez de Velasco Gonzales Vigil <87100608+rodrigovdv01@users.noreply.github.com>
2023-12-06 17:20:18 -0500

    06/12/2023 database

--- routes/PedidoRoutes.js
+++ routes/PedidoRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { createPedido, getMisPedidos, getDetalles, createDetallePedido, getAllPedidos, getPedidoById, updatePedidoById, deletePedidoById } from "../controllers/PedidoControllers.js";
+import { createPedido, getMisPedidos, getDetalles, getTransaccionPago, createDetallePedido, getAllPedidos, getPedidoById, updatePedidoById, deletePedidoById } from "../controllers/PedidoControllers.js";
 
 const router = express.Router();
 
 // Rutas para pedidos
 router.get('/', getAllPedidos); // Obtener todos los pedidos
-router.get('/mis-pedidos', getMisPedidos); // Obtener todos los pedidos
+router.get('/usuario/:id', getMisPedidos); // Obtener todos los pedidos
 router.get('/:id/detalles', getDetalles); // Obtener todos los pedidos
 router.get('/:id', getPedidoById); // Obtener un pedido por ID
+router.get('/:id/metodo-pago', getTransaccionPago);
 router.post('/crear-pedido', createPedido); // Crear un nuevo pedido
 router.post('/detalles-de-pedido', createDetallePedido); // 
 router.put('/:id', updatePedidoById); // Actualizar un pedido por ID
