75bc46bf6655dbfa916d7857702301a0c73dec89
Rodrigo Vásquez de Velasco Gonzales Vigil <87100608+rodrigovdv01@users.noreply.github.com>
2023-12-19 20:34:15 -0500

    19/12/2023

--- models/User.js
+++ models/User.js
@@ -39,7 +39,7 @@ const User = db.define(
     },
     distrito: {
       type: DataTypes.STRING,
-      allowNull: true,
+      allowNull: false,
     },
     level: {
       type: DataTypes.ENUM("user", "admin"), // Utilizando ENUM para valores específicos
